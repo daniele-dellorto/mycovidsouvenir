d3b66d7df6150f478f239fd2501def428df1de79
daniele-dellorto <37075250+daniele-dellorto@users.noreply.github.com>
2021-12-09 15:24:34 +0100

    graph

--- scripts/collection.js
+++ scripts/collection.js
@@ -21,7 +21,7 @@ d3.json("data/collections.json").then(function (categories) {
     let coverTxt = coverContainer.append('div')
         .classed("col-6", true);
 
-     coverTxt.append("h1")
+    coverTxt.append("h1")
         .classed("collectionTitle", true)
         .html(categories[collection].title)
 
@@ -35,6 +35,8 @@ d3.json("data/souvenirs.json").then(function (myDataRaw) {
 
     myData = [];
 
+    allCountries = [];
+
     //FOR EACH row verify if the product fits into the category
     myDataRaw.forEach(function (product) {
 
@@ -44,9 +46,45 @@ d3.json("data/souvenirs.json").then(function (myDataRaw) {
 
         //IF product includes the category -> add its data to myData (array with all products fitting)
         if (dataSemantics.includes(collection) || dataVisuals.includes(collection)) {
+
             myData.push(product)
+
+            countries = product.Country.split(', ').map(s => s.toLowerCase());
+            allCountries = allCountries.concat(countries);
+
         }
+
     })
+
+    console.log(allCountries);
+
+    setCountries = [...new Set(allCountries)];
+
+    console.log(setCountries);
+
+    var countCountries = [];
+
+    setCountries.forEach(function (country) {
+
+        var count = 0;
+
+        allCountries.forEach(function (newCountry) {
+
+            if (newCountry == country) {
+                count++
+            }
+
+        })
+
+        countCountries.push({
+            'name': country,
+            'value': count
+        })
+
+
+    })
+
+    console.log(countCountries);
 
     //object type
     const productType = ['bracelets', 'caps', 'flags', 'masks', 'mugs', 'patches', 'pins', 'socks', 'stickers', 't-shirts', 'others']
@@ -92,4 +130,63 @@ d3.json("data/souvenirs.json").then(function (myDataRaw) {
         }
 
     });
+
 })
+
+function createDonutChart() {
+
+    // set the dimensions and margins of the graph
+    var width = 450
+    height = 450
+    margin = 40
+
+    // The radius of the pieplot is half the width or half the height (smallest one). I subtract a bit of margin.
+    var radius = Math.min(width, height) / 2 - margin
+
+    // append the svg object to the div called 'my_dataviz'
+    var svg = d3.select("#countryContainer")
+        .append("svg")
+        .attr("width", width)
+        .attr("height", height)
+        .append("g")
+        .attr("transform", "translate(" + width / 2 + "," + height / 2 + ")");
+
+    // Create dummy data
+    var data = {
+        a: 9,
+        b: 20,
+        c: 30,
+        d: 8,
+        e: 12
+    }
+
+    // set the color scale
+    var color = d3.scaleOrdinal()
+        .domain(data)
+        .range(["#98abc5", "#8a89a6", "#7b6888", "#6b486b", "#a05d56"])
+
+    // Compute the position of each group on the pie:
+    var pie = d3.pie()
+        .value(function (d) {
+            return d.value;
+        })
+    var data_ready = pie(d3.entries(data))
+
+    // Build the pie chart: Basically, each part of the pie is a path that we build using the arc function.
+    svg
+        .selectAll('whatever')
+        .data(data_ready)
+        .enter()
+        .append('path')
+        .attr('d', d3.arc()
+            .innerRadius(100) // This is the size of the donut hole
+            .outerRadius(radius)
+        )
+        .attr('fill', function (d) {
+            return (color(d.data.key))
+        })
+        .attr("stroke", "black")
+        .style("stroke-width", "2px")
+        .style("opacity", 0.7)
+
+}
