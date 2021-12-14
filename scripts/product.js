c3da62f54a44b5c31121ba93f84f8beb58dd5715
daniele-dellorto <37075250+daniele-dellorto@users.noreply.github.com>
2021-12-14 15:36:54 +0100

    maledetta oceania

--- scripts/product.js
+++ scripts/product.js
@@ -9,31 +9,39 @@ d3.json("data/souvenirs.json").then(function (data) {
     //FOR EACH row verify if the product fits into the category
     for (var i = 0; i < data.length; i++) {
 
-        if (data[i].id == id) {            
+        if (data[i].id == id) {
 
             textContainer.append('h2')
                 .classed("product-name", true)
-                .html(data[i].name)            
+                .html(data[i].name)
 
             textContainer.append('h3')
                 .classed("product-category", true)
-                .html(data[i].category)            
+                .html(data[i].category)
 
             textContainer.append('p')
                 .classed("product-tags", true)
-                .html("message: " + data[i].semantic)            
+                .html("message: " + data[i].semantic)
 
             textContainer.append('p')
                 .classed("product-tags", true)
-                .html("message: " + data[i].visual)            
-
-                textContainer.append('p')
-                    .classed("product-tags", true)
-                    .html(data[i].country)
+                .html("message: " + data[i].visual)
 
             imgContainer.append('img')
                 .classed("product-image", true)
                 .attr("src", data[i].image)
+
+            countries = data[i].country.split(', ').map(s => s.toLowerCase());
+
+            countries.forEach(function (country) {
+
+                flagLink = "./assets/svg/" + country + ".svg"
+
+                textContainer.append('img')
+                    .classed("flag", true)
+                    .attr("src", flagLink)
+
+            })
 
             break
         }
